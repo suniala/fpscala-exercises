18549c57b8ba8ada5847779efa7a6271bfad91fa
Mikko Suniala <mikko.suniala@iki.fi>
2019-02-27 14:30:03 +0200

    Rest of chapter 3

--- src/main/scala/fi/kapsi/kosmik/fpscala/Chapter03.scala
+++ src/main/scala/fi/kapsi/kosmik/fpscala/Chapter03.scala
@@ -1,5 +1,7 @@
 package fi.kapsi.kosmik.fpscala
 
+import scala.annotation.tailrec
+
 object Chapter03 {
 
   object Ex13 {
@@ -14,6 +16,103 @@ object Chapter03 {
       */
     def foldRightFL[A, B](xs: List[A], z: B)(f: (A, B) => B): B =
       xs.reverse.foldLeft(z)((b, a) => f(a, b))
+  }
+
+  object Ex14 {
+    def appendFL[A](xs: List[A], item: A): List[A] =
+      xs.reverse.foldLeft(item :: Nil)((b, a) => a :: b)
+  }
+
+  object Ex15 {
+    /**
+      * Concatenate list of lists into a single list. Runtime should be linear in the total length of all lists.
+      */
+    def flatten[A](xss: List[List[A]]): List[A] =
+      xss.foldRight(List[A]())((xs, acc) => xs.foldRight(acc)((x, acc) => x :: acc))
+  }
+
+  object Ex24 {
+    /**
+      * A pure but not necessarily an efficient implementation.
+      */
+    def hasSubSequence[A](sup: List[A], sub: List[A]): Boolean = {
+      @tailrec
+      def hasPrefix[B](list: List[B], pref: List[B]): Boolean = {
+        pref match {
+          case Nil => true
+          case p :: ps =>
+            list match {
+              case Nil => false
+              case x :: xs => if (p == x) hasPrefix(xs, ps) else false
+            }
+        }
+      }
+
+      sup match {
+        case Nil => false
+        case x :: xs => if (x == sub.head) {
+          val m = hasPrefix(xs, sub.tail)
+          if (m) m else hasSubSequence(xs, sub)
+        } else {
+          hasSubSequence(xs, sub)
+        }
+      }
+    }
+  }
+
+  sealed trait Tree[+A]
+
+  case class Leaf[A](value: A) extends Tree[A]
+
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object Ex25 {
+    def size[A](tree: Tree[A]): Int =
+      tree match {
+        case Leaf(_) => 1
+        case Branch(l, r) => 1 + size(l) + size(r)
+      }
+  }
+
+  object Ex26 {
+    def maximum(tree: Tree[Int]): Int =
+      tree match {
+        case Leaf(v) => v
+        case Branch(l, r) => Math.max(maximum(l), maximum(r))
+      }
+  }
+
+  object Ex27 {
+    def depth[A](tree: Tree[A]): Int =
+      tree match {
+        case Leaf(_) => 1
+        case Branch(l, r) => 1 + Math.max(depth(l), depth(r))
+      }
+  }
+
+  object Ex28 {
+    def map[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+      tree match {
+        case Leaf(v) => Leaf(f(v))
+        case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+      }
+  }
+
+  object Ex29 {
+    def fold[A, B](tree: Tree[A])(fl: A => B, fb: (B, B) => B): B = {
+      tree match {
+        case Leaf(v) => fl(v)
+        case Branch(l, r) => fb(fold(l)(fl, fb), fold(r)(fl, fb))
+      }
+    }
+
+    def size[A](tree: Tree[A]): Int = fold[A, Int](tree)(_ => 1, (l, r) => 1 + l + r)
+
+    def maximum(tree: Tree[Int]): Int = fold[Int, Int](tree)(v => v, (l, r) => Math.max(l, r))
+
+    def depth[A](tree: Tree[A]): Int = fold[A, Int](tree)(_ => 1, (l, r) => 1 + Math.max(l, r))
+
+    def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = fold[A, Tree[B]](tree)(v => Leaf(f(v)), (l, r) => Branch(l, r))
   }
 
 }
--- src/test/scala/fi/kapsi/kosmik/fpscala/Chapter03Spec.scala
+++ src/test/scala/fi/kapsi/kosmik/fpscala/Chapter03Spec.scala
@@ -1,5 +1,6 @@
 package fi.kapsi.kosmik.fpscala
 
+import fi.kapsi.kosmik.fpscala.Chapter03.{Branch, Leaf}
 import org.scalatest.{FunSpec, Matchers}
 
 class Chapter03Spec extends FunSpec with Matchers {
@@ -25,4 +26,98 @@ class Chapter03Spec extends FunSpec with Matchers {
       foldLeftFR(list, Nil: List[Int])((acc, h) => h :: acc) shouldEqual List(3, 2, 1)
     }
   }
+
+  describe("Ex 14") {
+    import Chapter03.Ex14._
+
+    it("should append") {
+      appendFL(List(1, 2, 3), 4) shouldEqual List(1, 2, 3, 4)
+    }
+  }
+
+  describe("Ex 15") {
+    import Chapter03.Ex15._
+
+    it("should flatten") {
+      flatten(List(List(1), List(2, 3), List(), List(4, 5))) shouldEqual List(1, 2, 3, 4, 5)
+    }
+  }
+
+  describe("Ex 24") {
+    import Chapter03.Ex24._
+
+    it("should match") {
+      hasSubSequence(List(1), List(1)) shouldEqual true
+      hasSubSequence(List(1, 8, 2, 0), List(0)) shouldEqual true
+      hasSubSequence(List(1, 8, 2, 0), List(8, 2)) shouldEqual true
+      hasSubSequence(List(1, 8, 2, 0), List(1, 8, 2, 0)) shouldEqual true
+    }
+
+    it("should not match") {
+      hasSubSequence(List(2), List(1)) shouldEqual false
+      hasSubSequence(List(2), List(2, 2)) shouldEqual false
+      hasSubSequence(List(2, 5, 1, 6), List(5, 1, 1)) shouldEqual false
+    }
+  }
+
+  describe("Ex 25") {
+    import Chapter03.Ex25.{size => tsize}
+
+    it("should count size of tree") {
+      tsize(Leaf(1)) shouldEqual 1
+      tsize(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) shouldEqual 5
+    }
+  }
+
+  describe("Ex 26") {
+    import Chapter03.Ex26._
+
+    it("should get maximum value") {
+      maximum(Leaf(1)) shouldEqual 1
+      maximum(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) shouldEqual 3
+    }
+  }
+
+  describe("Ex 27") {
+    import Chapter03.Ex27._
+
+    it("should get depth") {
+      depth(Leaf(1)) shouldEqual 1
+      depth(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) shouldEqual 3
+    }
+  }
+
+  describe("Ex 28") {
+    import Chapter03.Ex28._
+
+    it("should map tree") {
+      map(Leaf(1))(_ * 2) shouldEqual Leaf(2)
+      map(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))))(_ * 2) shouldEqual Branch(Leaf(2), Branch(Leaf(4), Leaf(6)))
+    }
+  }
+
+  describe("Ex 29") {
+    import Chapter03.Ex29.{size => tsize, maximum, depth, map}
+
+    it("should count size of tree") {
+      tsize(Leaf(1)) shouldEqual 1
+      tsize(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) shouldEqual 5
+    }
+
+    it("should get maximum value") {
+      maximum(Leaf(1)) shouldEqual 1
+      maximum(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) shouldEqual 3
+    }
+
+    it("should get depth") {
+      depth(Leaf(1)) shouldEqual 1
+      depth(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) shouldEqual 3
+    }
+
+    it("should map tree") {
+      map(Leaf(1))(_ * 2) shouldEqual Leaf(2)
+      map(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))))(_ * 2) shouldEqual Branch(Leaf(2), Branch(Leaf(4), Leaf(6)))
+    }
+  }
+
 }
